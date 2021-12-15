86bae897bfc25e641c18fdc841c596dc47a40645
Pawan <pawandeepk@my.com>
2021-12-15 21:42:07 +0000

    Set up initial screens

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import ScreenOne from './src/screens/ScreenOne';
+ import ScreenTwo from './src/screens/ScreenTwo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ const App = () => {
+   return (
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName ="Index">
+         <Stack.Screen 
+           name="Index"
+           component={IndexScreen}
+           options={{ title: "Reading Diary" }}
+         />
+         <Stack.Screen 
+           name="ScreenOne"
+           component={ScreenOne}
+           options={{ title: "Screen 1" }}
+         />
+         <Stack.Screen 
+           name="ScreenTwo"
+           component={ScreenTwo}
+           options={{ title: "Screen 2" }}
+         />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ }
+
+export default App;
