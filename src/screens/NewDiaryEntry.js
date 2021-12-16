165de7f927dcb27b133f5a283fddbd1a49d8949e
Pawan <pawandeepk@my.com>
2021-12-16 15:01:25 +0000

    Configured useState hook

--- src/screens/NewDiaryEntry.js
+++ src/screens/NewDiaryEntry.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import { View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet} from 'react-native';
 import NavButton from '../components/NavButton';
 
  const NewDiaryEntry = ({navigation}) => {
-   return (
-     <View>
-        <NavButton screenName="Home" screenNav="Index" navigation={navigation} />
-         <NavButton screenName="History" screenNav="History" navigation={navigation} />
-         <Text>This is screen one :)</Text>
-     </View>
+    const [name, setName] = useState('');
+    return (
+       <View>
+          <NavButton screenName="Home" screenNav="Index" navigation={navigation} />
+           <NavButton screenName="History" screenNav="History" navigation={navigation} />
+
+          <Text style={styles.textLabel}>Enter your name:</Text>
+          <TextInput
+            style={styles.TextInput} 
+            placeholder="Type here"
+            value={name}
+            onChangeText={(text) => {
+              setName(text);
+            }} 
+          />
+          <Text style={styles.textLabel}>Your name is {name}</Text>
+        </View>
    );
  }
+
+ const styles = StyleSheet.create({
+   TextInput: {
+     fontSize: 20,
+     padding: 10,
+     margin: 5,
+     borderWidth: 1,
+   },
+   textLabel: {
+     fontSize: 18,
+     paddingLeft: 10,
+     marginTop: 10,
+   },
+ });
 
  export default NewDiaryEntry; 
